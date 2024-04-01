c70cf393cfad8936ed4ea79e18b0049245497a59
João Pedro <joaopedroc035@gmail.com>
2024-04-01 00:04:02 -0300

    feat: change sla oqeu

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Aceitas",
+  title: "e o treino fessora?",
   description: "Melhorou da Gripe????",
 };
 
